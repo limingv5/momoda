7e485bf079f3ce20df2d8cd9792f5b7d6169e32e
liming.mlm <liming.mlm@alibaba-inc.com>
2015-07-16 20:30:50 +0800

    0.1.1

--- controller/bind.js
+++ controller/bind.js
@@ -1,10 +1,11 @@
 var ROS = require("../lib/ros");
 var async = require("async");
 
-module.exports = function (serverIP, clientIP, cb) {
+module.exports = function (serverIP, clientIP, HTTP_PORT, HTTPS_PORT, cb) {
   var ros = new ROS();
   ros.action(function (chan) {
     var item = "/ip/firewall/nat";
+    var queue = "/queue/simple";
     var obj = {
       chain: "dstnat",
       action: "dst-nat",
@@ -16,20 +17,26 @@ module.exports = function (serverIP, clientIP, cb) {
     };
 
     var waterfallArr = [
-      //function (callback) {
-      //  ros.remove(chan, item, {"to-addresses": obj["to-addresses"]}, callback);
-      //},
       function (callback) {
-        ros.remove(chan, item, {"src-address": obj["src-address"]}, callback);
+        ros.remove(chan, item, {"src-address": clientIP}, callback);
+      },
+      function (chan, callback) {
+        ros.remove(chan, item, {"src-addresses": serverIP}, callback);
+      },
+      function (chan, callback) {
+        ros.remove(chan, queue, {target: clientIP + "/32"}, callback);
+      },
+      function (chan, callback) {
+        ros.remove(chan, queue, {target: serverIP + "/32"}, callback);
       },
       function (chan, callback) {
         obj["dst-port"] = 80;
-        obj["to-ports"] = 9080;
+        obj["to-ports"] = HTTP_PORT || 9080;
         ros.add(chan, item, obj, callback);
       },
       function (chan, callback) {
         obj["dst-port"] = 443;
-        obj["to-ports"] = 9443;
+        obj["to-ports"] = HTTPS_PORT || 9443;
         ros.add(chan, item, obj, callback);
       }
     ];
--- controller/clear.js
+++ controller/clear.js
@@ -4,6 +4,18 @@ var async = require("async");
 module.exports = function (serverIP, cb) {
   var ros = new ROS();
   ros.action(function (chan) {
-    ros.remove(chan, "/ip/firewall/nat", {"to-addresses": serverIP}, cb);
+    var waterfallArr = [
+      function (callback) {
+        ros.remove(chan, "/ip/firewall/nat", {"to-addresses": serverIP}, callback);
+      },
+      function (chan, callback) {
+        ros.remove(chan, "/queue/simple", {target: serverIP + "/32"}, callback);
+      }
+    ];
+
+    async.waterfall(waterfallArr, function (err, c) {
+      c.close();
+      cb && cb();
+    });
   });
 };
